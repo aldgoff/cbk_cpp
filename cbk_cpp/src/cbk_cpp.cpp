273e16cf446b080108bce3087b099095833c9eb0
Allan Goff <allangoff@gmail.com>
2014-10-23 10:20:00 -0700

    Initial executable.

---
+++ cbk_cpp/src/cbk_cpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : cbk_cpp.cpp
+// Author      : Allan Goff
+// Version     :
+// Copyright   : Novatia Labs 2014
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
